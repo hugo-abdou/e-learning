c6057ff05b88fe6ede8e50ca167d21d047a125c1
Abdelghafour <44982282+hugo-abdou@users.noreply.github.com>
2024-02-01 16:48:50 +0100

    update server

--- deploy.sh
+++ deploy.sh
@@ -3,28 +3,26 @@
 GITHUB="hugo-abdou/e-learning"
 BRANCH="main"
 
-SSH_KEY="./id_rsa"
+# SSH_KEY="~/.ssh/id_rsa"
 WORK_TREE="./"
 GIT_DIR="./.git"
-chmod 600 $SSH_KEY
-eval $(ssh-agent -s)
-ssh-add $SSH_KEY
+# chmod 600 $SSH_KEY
+eval $(sudo ssh-agent -s)
+# sudo ssh-add $SSH_KEY
 REPO="git@github.com:$GITHUB"
 if [ -d "$GIT_DIR" ]; then
     cd $WORK_TREE
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch origin --tags --force
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR checkout -f $BRANCH
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR merge origin/$BRANCH
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch origin --tags --force
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR checkout -f $BRANCH
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR merge origin/$BRANCH
 else
-    git init --bare $GIT_DIR
-    rm -rf $WORK_TREE
-    mkdir $WORK_TREE
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR remote add origin $REPO
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch origin --tags --force
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR checkout -f $BRANCH
-    git --work-tree=$WORK_TREE --git-dir=$GIT_DIR merge origin/$BRANCH
+    sudo git init --bare $GIT_DIR
+    sudo rm -rf $WORK_TREE
+    sudo mkdir $WORK_TREE
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR remote add origin $REPO
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR fetch origin --tags --force
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR checkout -f $BRANCH
+    sudo git --work-tree=$WORK_TREE --git-dir=$GIT_DIR merge origin/$BRANCH
 fi
-
- 
