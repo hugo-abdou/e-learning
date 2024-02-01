930e4f7a12ce03ca8dfdc6d6f93cf532f1b09da7
Abdelghafour <44982282+hugo-abdou@users.noreply.github.com>
2024-02-01 16:05:46 +0100

    update server

--- app/Http/Controllers/GitHubWebhookController.php
+++ app/Http/Controllers/GitHubWebhookController.php
@@ -16,7 +16,7 @@ class GitHubWebhookController extends Controller
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
         if (hash_equals($githubHash, $localHash)) {
             $root_path = base_path();
-            $process = (new Process(['./deploy.sh']))->set_include_path($root_path);
+            $process = (new Process(['./deploy.sh']));
             $process->run(function ($type, $buffer) {
                 Log::info($buffer);
             });
